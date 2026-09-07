fc2cb336ab83670e112093e8c9d5b63e8a184140
Moritisimor <devMoritisimor@proton.me>
2026-09-07 16:53:53 +0200

    Implemented anonymous functions

--- jomad/src/main/kotlin/jomad/prelude/Functions.kt
+++ jomad/src/main/kotlin/jomad/prelude/Functions.kt
@@ -4,32 +4,45 @@ import jomad.exceptions.EvaluationException
 import jomad.expressions.Expression
 import jomad.values.Environment
 import jomad.values.Value
+import jomad.values.newUnit
 
 fun registerFunctionalFunctions(env: Environment) {
-    env.registerNative("letfun", fun(args: List<Expression>, env: Environment): Result<Value> {
+    env.registerNativeThrowing("letfun", fun(args: List<Expression>, env: Environment): Value {
         if (args.size != 3)
-            return Result.failure(EvaluationException("letfun expects 3 arguments"))
+            throw EvaluationException("letfun expects 3 arguments")
 
         val funName = args[0].getSymbolOrThrow()
         val paramList = args[1].getListLiteralOrThrow()
         val funParams = mutableListOf<String>()
 
-        for (param in paramList) {
-            when (param) {
-                is Expression.Symbol -> funParams.add(param.name)
-                else -> return Result.failure(EvaluationException(
-                    "Expected symbol expression in parameter list"
-                ))
-            }
-        }
+        for (param in paramList)
+            funParams.add(param.getSymbolOrThrow())
 
         val funBody = args[2]
-        env.setBinding(funName, Value.ValLambda(
+        env.setBindingOrThrow(funName, Value.ValLambda(
             parameters = funParams.toList(),
             body = funBody,
             captured = env
-        )).onFailure { return Result.failure(it) }
+        ))
 
-        return Result.success(Value.ValUnit)
+        return newUnit()
+    })
+
+    env.registerNativeThrowing("lambda", fun(args: List<Expression>, env: Environment): Value {
+        if (args.size != 2)
+            throw EvaluationException("lambda expects 2 arguments")
+
+        val paramList = args[0].getListLiteralOrThrow()
+        val body = args[1]
+        val funParams = mutableListOf<String>()
+
+        for (param in paramList)
+            funParams.addLast(param.getSymbolOrThrow())
+
+        return Value.ValLambda(
+            parameters = funParams.toList(),
+            body = body,
+            captured = env
+        )
     })
 }
